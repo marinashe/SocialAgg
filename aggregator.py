a573e4224a426b43affee39cffcd39230182ea0b
Marina Sheenkova <msheenkova@gmail.com>
2016-05-24 10:18:58 +0300

    added templates

--- aggregator.py
+++ aggregator.py
@@ -5,8 +5,10 @@ from pprint import pprint
 import dateutil.parser
 import datetime
 
+
 def get_name(name):
     return "<h3>{}</h3>".format(name)
+
 
 def get_list(list_arg):
     rezult = "<ul>"
@@ -14,40 +16,55 @@ def get_list(list_arg):
         rezult += "<li>{}</li>".format(li)
     return rezult + "</ul>"
 
-def show_posts(date = False):
+
+def show_posts(date=False):
     client = pymongo.MongoClient()
     db = client.get_database('socialagg')
-    rezult = ''
+    rezult = []
     pages = db.get_collection('pages')
     posts = db.get_collection('posts')
     if date:
         date = dateutil.parser.parse(date, ignoretz=True).date()
-        print(date)
         for page in pages.find():
             rez = []
-            rezult += get_name(page['name'])
             for post in sorted([p for p in [x for x in posts.find({'id': page['id']})][0]['posts'] if p[0].date() == date], reverse=True):
-                rez.append(str(post[0]) + ': ' + post[1])
-            rezult += get_list(rez)
+                rez.append({'time': str(post[0]), 'message': post[1]})
+            if rez:
+                rezult.append({'name': page['name'], 'posts': rez})
     else:
         for page in pages.find():
             rez = []
-            rezult += get_name(page['name'])
             for post in sorted([x for x in posts.find({'id': page['id']})][0]['posts'], reverse=True)[:50]:
-                rez.append(str(post[0]) + ': ' + post[1])
-            rezult += get_list(rez)
+                rez.append({'time': str(post[0]), 'message': post[1]})
+            if rez:
+                rezult.append({'name': page['name'], 'posts': rez})
     return rezult
 
+
 def show_pages():
-    rez = ''
+    rez = []
     client = pymongo.MongoClient()
     db = client.get_database('socialagg')
 
     pages = db.get_collection('pages')
 
     for page in pages.find():
-        rez += get_name(page['name']) + get_list(['About: {}'.format(page['about']), 'Fans: {}'.format(page['fans'])])
+        rez.append({'name': page['name'], 'about': page['about'], 'fans': page['fans'], 'best': bests(page['id'])})
 
+    return rez
+
+
+def bests(page_id):
+    client = pymongo.MongoClient()
+    db = client.get_database('socialagg')
+    pages = db.get_collection('pages')
+    posts = db.get_collection('posts')
+
+    rez = []
+    allposts = [x for x in posts.find({'id': page_id})][0]['posts']
+
+    for post in sorted(allposts, key=lambda likes: likes[3], reverse=True)[:3]:
+        rez.append({'time': str(post[0]), 'like': post[3], 'message':post[1]})
     return rez
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -6,17 +6,17 @@ import aggregator
 
 @route('/')
 def index():
-    return aggregator.show_pages()
+    return template('index.html', pages=aggregator.show_pages())
 
 
 @route('/latest/')
 def index():
-    return aggregator.show_posts()
+    return template('latest.html', pages=aggregator.show_posts(), title='Latest posts')
 
 
 @route('/<year>/<month>/<day>/')
 def index(year, month, day):
-    return aggregator.show_posts(year +'/' +month + '/' + day)
+    return template('latest.html', pages=aggregator.show_posts(year + '/' + month + '/' + day), title='Posts {}/{}/{}'.format(year, month, day))
 
 if __name__ == "__main__":
     run(debug=True, reloader=True)
